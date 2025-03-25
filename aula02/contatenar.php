94e7a85d8e119188588d64ab1e5320aed5813fb1
Matheus Bailon <matheus.09@gmail.com>
2025-03-25 16:31:04 -0300

    Aula sobre concatenação em php

---
+++ aula02/contatenar.php
@@ -0,0 +1,15 @@
+<?php
+    ECHO "hello world<br>";
+    echo "hello world<br>";
+    EcHo "hello world<br>";
+
+    $nome = "Matheus";
+    $sobrenome = "Bailon";
+    $numero = 4;
+
+    echo $nome . "lindo" . "<br>";
+    echo "${nome} <h1> ${sobrenome} </h1>";
+    echo $numero . "<p> " . $nome . 
+    " " . $sobrenome . "</p>";
+
+?>
